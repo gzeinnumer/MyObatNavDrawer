82529154b98659740e8a4e2212ed462d5ac32b62
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-10 22:18:19 +0700

    afasda

---
+++ app/src/main/java/com/gzeinnumer/myobatnavdrawer/model/ResponseReadObat.java
@@ -0,0 +1,116 @@
+package com.gzeinnumer.myobatnavdrawer.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseReadObat{
+
+	@SerializedName("Satuan")
+	private String satuan;
+
+	@SerializedName("UnitID")
+	private String unitID;
+
+	@SerializedName("BrgID")
+	private String brgID;
+
+	@SerializedName("BrgName")
+	private String brgName;
+
+	@SerializedName("Jam")
+	private String jam;
+
+	@SerializedName("LokasiName")
+	private String lokasiName;
+
+	@SerializedName("Qty")
+	private int qty;
+
+	@SerializedName("Tgl")
+	private String tgl;
+
+	@SerializedName("LokasiID")
+	private String lokasiID;
+
+	@SerializedName("UnitName")
+	private String unitName;
+
+	public void setSatuan(String satuan){
+		this.satuan = satuan;
+	}
+
+	public String getSatuan(){
+		return satuan;
+	}
+
+	public void setUnitID(String unitID){
+		this.unitID = unitID;
+	}
+
+	public String getUnitID(){
+		return unitID;
+	}
+
+	public void setBrgID(String brgID){
+		this.brgID = brgID;
+	}
+
+	public String getBrgID(){
+		return brgID;
+	}
+
+	public void setBrgName(String brgName){
+		this.brgName = brgName;
+	}
+
+	public String getBrgName(){
+		return brgName;
+	}
+
+	public void setJam(String jam){
+		this.jam = jam;
+	}
+
+	public String getJam(){
+		return jam;
+	}
+
+	public void setLokasiName(String lokasiName){
+		this.lokasiName = lokasiName;
+	}
+
+	public String getLokasiName(){
+		return lokasiName;
+	}
+
+	public void setQty(int qty){
+		this.qty = qty;
+	}
+
+	public int getQty(){
+		return qty;
+	}
+
+	public void setTgl(String tgl){
+		this.tgl = tgl;
+	}
+
+	public String getTgl(){
+		return tgl;
+	}
+
+	public void setLokasiID(String lokasiID){
+		this.lokasiID = lokasiID;
+	}
+
+	public String getLokasiID(){
+		return lokasiID;
+	}
+
+	public void setUnitName(String unitName){
+		this.unitName = unitName;
+	}
+
+	public String getUnitName(){
+		return unitName;
+	}
+}
